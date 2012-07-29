28f0905c8043faeee3ffa85dcfe755cff5b33298
Barrett Lewis <musikal.fusion@gmail.com>
2012-07-29 00:27:32 -0700

    Removed doxy files from branch

--- Doxy/html/search/functions_6d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['modifyitem',['ModifyItem',['../classbumodel_1_1_model.html#aba9ece059cb2c118b3dbdaaa88e4e096',1,'bumodel::Model']]]
-];
--- Doxy/html/search/variables_5f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_5fxmltree',['_XmlTree',['../classbumodel_1_1_model.html#a56a5a93332501233f3d88da4961e119b',1,'bumodel::Model']]]
-];
