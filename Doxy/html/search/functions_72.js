58572be6a0fe01f3f6ea7b729706a0eb304d17af
Barrett Lewis <musikal.fusion@gmail.com>
2012-07-29 01:46:51 -0700

    Ran Doxygen
    Hopefully the server can find the index this time.

---
+++ Doxy/html/search/functions_72.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['removebackupitem',['RemoveBackUpItem',['../classbumodel_1_1_model.html#a39d52acd9a7da0540da5b9f8780c5ec9',1,'bumodel::Model']]]
+];
---
+++ html/search/functions_6d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['modifyitem',['ModifyItem',['../classbumodel_1_1_model.html#aba9ece059cb2c118b3dbdaaa88e4e096',1,'bumodel::Model']]]
+];
